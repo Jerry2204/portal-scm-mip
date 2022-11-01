bcd5259866efc51811f97f4aa724e94adcd0185f
Jerry Andrianto Pangaribuan <jerryandrianto22@gmail.com>
2022-11-01 20:02:55 +0700

    delete notification

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -26,48 +26,6 @@
                 </li>
             </ul>
             <ul class="nav-right">
-                <li class="header-notification">
-                    <a href="#!">
-                        <i class="ti-bell"></i>
-                        <span class="badge bg-c-pink"></span>
-                    </a>
-                    <ul class="show-notification">
-                        <li>
-                            <h6>Notifications</h6>
-                            <label class="label label-danger">New</label>
-                        </li>
-                        <li>
-                            <div class="media">
-                                <img class="d-flex align-self-center img-radius" src="{{ asset('admin/images/avatar-4.jpg') }}" alt="Generic placeholder image">
-                                <div class="media-body">
-                                    <h5 class="notification-user">John Doe</h5>
-                                    <p class="notification-msg">Lorem ipsum dolor sit amet, consectetuer elit.</p>
-                                    <span class="notification-time">30 minutes ago</span>
-                                </div>
-                            </div>
-                        </li>
-                        <li>
-                            <div class="media">
-                                <img class="d-flex align-self-center img-radius" src="{{ asset('admin/images/avatar-3.jpg') }}" alt="Generic placeholder image">
-                                <div class="media-body">
-                                    <h5 class="notification-user">Joseph William</h5>
-                                    <p class="notification-msg">Lorem ipsum dolor sit amet, consectetuer elit.</p>
-                                    <span class="notification-time">30 minutes ago</span>
-                                </div>
-                            </div>
-                        </li>
-                        <li>
-                            <div class="media">
-                                <img class="d-flex align-self-center img-radius" src="{{ asset('admin/images/avatar-4.jpg') }}" alt="Generic placeholder image">
-                                <div class="media-body">
-                                    <h5 class="notification-user">Sara Soudein</h5>
-                                    <p class="notification-msg">Lorem ipsum dolor sit amet, consectetuer elit.</p>
-                                    <span class="notification-time">30 minutes ago</span>
-                                </div>
-                            </div>
-                        </li>
-                    </ul>
-                </li>
                 <li class="user-profile header-notification">
                     <a href="#" class="d-flex align-items-center">
                         <div class="d-flex justify-content-center align-items-center word-image-navbar text-white bg-c-blue mr-3">
